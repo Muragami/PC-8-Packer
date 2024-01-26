e568966d7dd2650946be09304c1ab878ac508472
JasonP <muragami@wishray.com>
2024-01-26 16:41:52 -0500

    Increase screen size

--- conf.lua
+++ conf.lua
@@ -4,7 +4,7 @@
 
 function love.conf(t)
 	t.window.title = "PC-8 Packer"
-	t.window.width = 1000
+	t.window.width = 1200
 	t.window.height = 700
 	t.window.resizable = false
 	t.identity = "pc8_packer"
